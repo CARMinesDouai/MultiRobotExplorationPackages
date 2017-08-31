33ef897aa84214362e7baa4e290b6e7e8fac04ab
lxsang <xsang.le@gmail.com>
2017-08-31 14:08:39 +0200

    fix network pb

--- multi_master_bridge/src/map_feeding.cpp
+++ multi_master_bridge/src/map_feeding.cpp
@@ -88,48 +88,17 @@ int main(int argc, char **argv)
 	neighbors.size = 0;
 	//pub = n.advertise<nav_msgs::OccupancyGrid>("other_map", 1000);
 	ros::Subscriber sub = n.subscribe<multi_master_bridge::NeighbourList>("/new_robot", 50,&neighbors_discover);
-	ros::Subscriber sub1 = n.subscribe<multi_master_bridge::MapData>(map_update_, 100,&send_newmap);
+	ros::Subscriber sub1 = n.subscribe<multi_master_bridge::MapData>(map_update_, 1,&send_newmap);
 	ros::Rate loop_rate(_sending_rate);
 	id = read_inet_id(_interface.c_str());
-	ROS_INFO("My address %s on %s",inet_ntoa(id.ip), _interface.c_str());
+	ROS_INFO("My address %s on %s, publish rate: %f",inet_ntoa(id.ip), _interface.c_str(), _sending_rate);
 	while(ros::ok())
 	{
-		ros::spinOnce();
 		loop_rate.sleep();
+		ros::spinOnce();
 	}
     
 
 	return 0;
     
 }
-/*
-nav_msgs::OccupancyGrid m;
-	m.header.seq = 10;
-	m.header.stamp = ros::Time::now();
-	m.header.frame_id = "feeder";
-	m.info.map_load_time = ros::Time::now();
-	m.info.resolution = 0.1;
-	m.info.width = 480;
-	m.info.height = 320;
-	m.info.origin.position.x = 1.2;
-	m.info.origin.position.y = 1.4;
-	m.info.origin.position.z = 1.7;
-	m.info.origin.orientation.x = 0.7;
-	m.info.origin.orientation.y = 0.45;
-	m.info.origin.orientation.z = 40.56;
-	m.info.origin.orientation.w = 45.6666;
-	int i=0;
-	for(i;i<255;i++) m.data.push_back(i+1);
-	OccupancyGridHelper hp;
-	hp.consume((void*)&m);
-    struct portal_data_t d = hp.getPortalDataFor("10.1.16.45");
-	d.publish_to = "/food_chain";
-	d.hash = OccupancyGridHelper::hash();
-
-	//ROS_INFO("Feed x:%f, st.sec:%d, st.nsec:%d, fid:%s", m.seq, m.stamp.sec,m.stamp.nsec,m.frame_id.c_str());
-	pub.publish(m);
-	teleport_raw_data(msg->ip.c_str(),msg->port,d);
-	
-	// free allocated memory
-	if(d.data) free(d.data);
-*/
