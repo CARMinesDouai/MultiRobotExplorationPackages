f7d7e9cd034e598b06788e4ce84a74a2dfc9e270
Xuan Sang LE <xsang.le@gmail.com>
2018-01-04 18:01:07 +0100

    karto result

--- gazebo_sim/launch/rsload.sh
+++ gazebo_sim/launch/rsload.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 term=$1
-ps -p `ps -e | grep "$1" | awk '{print $1}'` -o time,%cpu,%mem >> $HOME/$1.rst
-sleep 5
+while :
+do
+    ps -p `ps -e | grep "$1" | awk '{print $1}'` -o time,%cpu,%mem >> $HOME/$1.rst
+    sleep 5
+done
